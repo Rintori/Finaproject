d6c2378fd20075d1910191809536a16bfa4d818e
cherrystz <plug4869@gmail.com>
2021-11-09 00:33:47 +0700

    v0.1 add all update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import Drawer from './components/Drawer'
-import Login from './components/Login'
-import SignUp from './components/Signup';
-import './App.css';
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
+import Drawer from "./components/Drawer";
+import Login from "./components/Login";
+import SignUp from "./components/Signup";
+
+const outerTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#FFAA00",
+    },
+  },
+});
+
+const innerTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#00FF00",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={outerTheme}>
       <Drawer />
-      <Routes>
-        <Route path="/" />
-        <Route path="/Login" element={<Login/>} />
-        <Route path="/Signup" element={<SignUp/>} />
-      </Routes>
-    </div>
+      <ThemeProvider theme={innerTheme}>
+        <Routes>
+          <Route exact path="/" />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<SignUp />} />
+        </Routes>
+      </ThemeProvider>
+    </ThemeProvider>
   );
 }
 
